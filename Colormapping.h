104733843eaa7da99df83239b764fa392c9f2aa3
YUK5COB <K.Yuvaraj@in.bosch.com>
2024-07-21 12:29:21 +0530

    Separate file to reduce LoC

---
+++ Colormapping.h
@@ -0,0 +1,22 @@
+#ifndef COLOR_H
+#define COLOR_H
+
+
+enum MajorColor {WHITE, RED, BLACK, YELLOW, VIOLET};
+enum MinorColor {BLUE, ORANGE, GREEN, BROWN, SLATE};
+
+extern const char* MajorColorNames[];
+extern int numberOfMajorColors;
+extern const char* MinorColorNames[];
+extern int numberOfMinorColors;
+const int MAX_COLORPAIR_NAME_CHARS = 16;
+
+typedef struct {
+    enum MajorColor majorColor;
+    enum MinorColor minorColor;
+} ColorPair;
+
+void ColorPairToString(const ColorPair* colorPair, char* buffer);
+ColorPair GetColorFromPairNumber(int pairNumber);
+int GetPairNumberFromColor(const ColorPair* colorPair);
+#endif
---
+++ colorcode_test.c
@@ -0,0 +1,45 @@
+#include <stdio.h>
+#include <assert.h> 
+#include "Colormapping.h"
+
+void PrintColorPair(int pairNumber, const ColorPair* colorPair, int show_pair_or_number)
+{
+    char colorPairNames[MAX_COLORPAIR_NAME_CHARS];
+    ColorPairToString(colorPair, colorPairNames);
+    if (show_pair_or_number) 
+    {
+        printf("Got pair number %d: %s\n", pairNumber, colorPairNames);
+    } else 
+    {
+        printf("Got pair %s\n", colorPairNames);
+    }
+}
+
+void testNumberToPair(int pairNumber,enum MajorColor expectedMajor,enum MinorColor expectedMinor)
+{
+    ColorPair colorPair = GetColorFromPairNumber(pairNumber);
+    PrintColorPair(pairNumber,&colorPair,0);
+    printf("Got pair %s\n", colorPairNames);
+    assert(colorPair.majorColor == expectedMajor);
+    assert(colorPair.minorColor == expectedMinor);
+}
+
+void testPairToNumber(enum MajorColor major,enum MinorColor minor,int expectedPairNumber)
+{
+    ColorPair colorPair;
+    colorPair.majorColor = major;
+    colorPair.minorColor = minor;
+    int pairNumber = GetPairNumberFromColor(&colorPair);
+    PrintColorPair(pairNumber,&colorPair,1);
+    assert(pairNumber == expectedPairNumber);
+}
+
+int main() {
+    testNumberToPair(4, WHITE, BROWN);
+    testNumberToPair(5, WHITE, SLATE);
+
+    testPairToNumber(BLACK, ORANGE, 12);
+    testPairToNumber(VIOLET, SLATE, 25);
+
+    return 0;
+}
